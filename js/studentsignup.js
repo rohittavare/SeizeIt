2ff2431f37fc448e6f2434259da42051a1d2b5d6
vybhavb <vybs2499@gmail.com>
2016-06-26 09:59:32 -0700

    FIXED! maybe

--- js/studentsignup.js
+++ js/studentsignup.js
@@ -1,18 +1,22 @@
 window.onload = function() {
 
-    var name = document.getElementById("name").value,
-        age = document.getElementById("age").value,
-        gpa = document.getElementById("gpa").value,
-        email = document.getElementById("email").value,
-        database = firebase.database();
+    var btn = document.getElementById("createAcc");
 
-    document.getElementById('createAcc').addEventListener('click',function () {
+    btn.addEventListener('click',function () {
 
-        firebase.auth().createUserWithEmailAndPassword(email.value,passwordGen()).catch(function(error) {
-            // Handle Errors here.
+        var name = document.getElementById("name"),
+            age = document.getElementById("age"),
+            gpa = document.getElementById("gpa"),
+            email = document.getElementById("email");
+
+        firebase.auth().createUserWithEmailAndPassword(email.value, passwordGen()).catch(function(error) {
+
             var errorCode = error.code;
             var errorMessage = error.message;
-            // ...
+
+            console.log("Error: " + errorCode + " message: " + errorMessage);
+            alert("An error has occurred.")
+
         });
 
         firebase.auth().onAuthStateChanged(function (user) {
@@ -34,12 +38,6 @@ window.onload = function() {
                 }, function(error) {
                     alert("An error has occoured.");
                 });
-        
-        firebase.auth().onAuthStateChanged(function(user) {
-            if (user) {
-                console.log("pushed");
-            } else {
-                // No user is signed in.
             }
         });
 
@@ -57,48 +55,3 @@ function passwordGen() {
     return password;
 
 }
-
-
-
-
-//
-// var btn = document.getElementById("createAcc");
-//
-// btn.addEventListener('click',function () {
-//
-//     var name = document.getElementById("name"),
-//         age = document.getElementById("age"),
-//         gpa = document.getElementById("gpa"),
-//         email = document.getElementById("email");
-//
-//     firebase.auth().createUserWithEmailAndPassword(email.value, passwordGen()).catch(function(error) {
-//
-//         var errorCode = error.code;
-//         var errorMessage = error.message;
-//
-//         console.log("Error: " + errorCode + " message: " + errorMessage);
-//         alert("An error has occurred.")
-//
-//     });
-//
-//     firebase.auth().onAuthStateChanged(function (user) {
-//         if(user){
-//             firebase.auth().sendPasswordResetEmail(user.email).then(function() {
-//                 // Email sent.
-//                 firebase.database().ref().push({
-//                     name: name.value,
-//                     age: age.value,
-//                     gpa: gpa.value,
-//                     email: email.value,
-//                     type: "student"
-//                 });
-//                 firebase.auth().signOut();
-//                 alert("Please check your email.");
-//                 window.location = "loginStudent.html";
-//             }, function(error) {
-//                 alert("An error has occoured.");
-//             });
-//         }
-//     });
-//
-// })
