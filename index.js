ae51b011764af5af96c497c1863eaef0e6c7b230
vybhavb <vybs2499@gmail.com>
2016-06-25 21:33:35 -0700

    Adding the Firebase API keys to work with firebase since AWS was not working

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-$(document).ready(function() {
+var config = {
+    apiKey: "AIzaSyCG36qDp3oC2H3BpqpPBMuLgrxGlKvpvWI",
+    authDomain: "seizeit-cdb9a.firebaseapp.com",
+    databaseURL: "https://seizeit-cdb9a.firebaseio.com",
+    storageBucket: "seizeit-cdb9a.appspot.com",
+  };
+  firebase.initializeApp(config);
   
-});
