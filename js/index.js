a707358525216e8184469d24b081477682f4aaad
vybhavb <vybs2499@gmail.com>
2016-06-25 22:34:43 -0700

    no Changes

--- js/index.js
+++ js/index.js
@@ -3,6 +3,5 @@ var config = {
     authDomain: "seizeit-cdb9a.firebaseapp.com",
     databaseURL: "https://seizeit-cdb9a.firebaseio.com",
     storageBucket: "seizeit-cdb9a.appspot.com",
-  };
+};
   firebase.initializeApp(config);
-  
