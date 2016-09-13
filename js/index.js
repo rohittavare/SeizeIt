82f8e5e83e04b46da49faea159cf04136d468684
vybhavb <vybs2499@gmail.com>
2016-09-13 14:34:31 -0700

    kewl changes ;)

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
-var /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/ config = /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/ {apiKey: "AIzaSyCG36qDp3oC2H3BpqpPBMuLgrxGlKvpvWI", /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/authDomain: "seizeit-cdb9a.firebaseapp.com", databaseURL: "https://seizeit-cdb9a.firebaseio.com", storageBucket: "seizeit-cdb9a.appspot.com"/*GO VBshdukfsadkjgfhgsadkjfhksdahfjkashdjghdfawe*/,};/*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshdsajfksadbjfsadhffklsd*/firebase.initializeApp(config)/*THANKSsdfasdklf jasdkfjkas kljdsaf jsdaASHWIN!!!!jskdlafhadsjfhaskjfhksjldfhjksdafh sdjfhskdja hkehfkja sdhfklah fjds hfjksd*/;window.onload = function() {$(".button-collapse").sideNav();document.getElementById("signOut").addEventListener('click',function () {if(firebase.auth().currentUser){firebase.auth().signOut();}});}
+var /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/ con
+fig = /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/ {ap
+iKey: "AIzaSyCG36qDp3oC2H3BpqpPBMuLgrxGlKvpvWI", /*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshfklsd*/authDomain: 
+"seizeit-cdb9a.firebaseapp.com", 
+databaseURL: 
+"https://seizeit-cdb9a.firebaseio.com", storageBucket: "seizeit-cdb9a.appspot.com"/*GO VBshdukfsadkjgfhgsadkjfhksdahfjkashdjghdfawe*/,
+};
+/*jkhfdahaskdljfasdkjkjkjhfsjkdfhsdjafhkjsadhfklsdahfjkadshdsajfksadbjfsadhffklsd*/firebase.initializeApp(config)/*THANKSsdfasdklf jasdkfjkas kljdsaf jsdaASHWIN!!!!jskdlafhadsjfhaskjfhksjldfhjksdafh sdjfhskdja hkehfkja sdhfklah fjds hfjksd*/;
+window.onload = function() {$(".button-collapse").sideNav();document
+.getElementById("signOut").addEventListener('click',function () {if(firebase.auth().currentUser){firebase.auth().signOut();}});}
